2832a76cfe1e3c9222ed1ca5f8405afd3093be2d
James Asher <onijim@gmail.com>
2014-08-23 19:17:00 -0400

    refactor image spec

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -2,30 +2,32 @@ require 'rails_helper'
 
 describe Image do
 
-  before(:each) do
-    @image = create(:image)
-  end
+  before { @image = create(:image) }
+  let(:image) { @image }
+  subject { image }
 
   it "should have a valid factory" do
     expect(FactoryGirl.create(:image)).to be_valid
   end
 
-  it "should be invalid without a title" do
-    @image.title = nil
+  context "should be invalid" do
+    it "without a title" do
+      image.title = nil
 
-    expect(@image).not_to be_valid
-  end
+      expect(image).not_to be_valid
+    end
 
-  it "should be invalid without an uploaded filename" do
-    @image.uploaded_filename = nil
+    it "without an uploaded filename" do
+      image.uploaded_filename = nil
 
-    expect(@image).not_to be_valid
-  end
+      expect(image).not_to be_valid
+    end
 
-  it "should be invalid without an original filename" do
-    @image.original_filename = nil
+    it "without an original filename" do
+      image.original_filename = nil
 
-    expect(@image).not_to be_valid
+      expect(image).not_to be_valid
+    end
   end
 
   it "should know how many tags it has" do
