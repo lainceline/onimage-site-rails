3e8753613ff176f9560a0cda026de8311bac0819
James Asher <onijim@gmail.com>
2014-08-24 01:11:10 -0400

    Index method on usercontroller returns all users.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe UsersController, :type => :controller do
       for i in 0..2
         create(:user)
       end
-      get '/users'
+      get :index
       expect(response).to be_success
       users = JSON.parse(response.body, symbolize_names: true)
       expect(users.count).to eq 3
