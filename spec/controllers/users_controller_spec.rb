2c9dcdd7948e79b1b50773b8c6180b72022181c0
James Asher <onijim@gmail.com>
2014-08-24 01:18:07 -0400

    Refactor the UserController tests to be in a better format

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
-
-  context "it should return json" do
-    it "of all users" do
+  describe '#index' do
+    it 'should return all users as json' do
       for i in 0..2
         create(:user)
       end
@@ -12,10 +11,11 @@ RSpec.describe UsersController, :type => :controller do
       users = JSON.parse(response.body, symbolize_names: true)
       expect(users.count).to eq 3
     end
+  end
 
-    it "for one user" do
+  describe '#show' do
+    it 'should return the user with the passed in id' do
       user = create(:user)
-
       get :show, {:id => user.id}
       expect(response).to be_success
     end
