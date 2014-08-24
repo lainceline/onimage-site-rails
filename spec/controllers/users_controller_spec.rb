5d6eebaa5b372691d6d224bb998ac2bb54bfa13e
James Asher <onijim@gmail.com>
2014-08-24 00:54:02 -0400

    User test for retrieving all users as json

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
 
+  context "it should return json" do
+    it "of all users" do
+      for i in 0..2
+        create(:user)
+      end
+      get '/users'
+      expect(response).to be_success
+      users = JSON.parse(response.body, symbolize_names: true)
+      expect(users.count).to eq 3
+    end
+  end
 end
