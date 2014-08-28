2e347464ef6c188358d36e9a3c5d1fdef8d20a79
James Asher <onijim@gmail.com>
2014-08-28 19:49:56 -0400

    Test refactoring, plus tests and code for Image#show action

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -3,30 +3,54 @@ require 'rails_helper'
 describe ImagesController do
 
   describe "#index" do
-    it "returns all images as json" do
+    before do
+      3.times { FactoryGirl.create(:image) }
+      get :index
+    end
 
-      3.times do
-        FactoryGirl.create(:image)
+    it "makes a successful http request" do
+      expect(response).to be_success
+    end
+
+    describe "it returns" do
+      subject(:images) { parse_json[:images] }
+
+      specify "images as json" do
+        expect(images.first).to have_key(:id)
       end
 
-      get :index
-
-      expect(response).to be_success
-      images = parse_json[:images]
-      expect(images.first).to have_key(:id)
-      expect(images.size).to eq 3
+      specify  "all of the images" do
+        expect(images.size).to eq 3
+      end
     end
   end
 
   describe "#show" do
-    it "returns the image with the passed in id" do
-      image = FactoryGirl.create(:image)
+    subject(:image) { FactoryGirl.create(:image) }
 
+    it "makes a successful http request" do
       get :show, { :id => image.id }
-
       expect(response).to be_success
-      image_response = parse_json[:image]
-      expect(image_response[:id]).to eq image.id
     end
+
+    context "returns an image" do
+      specify "with the passed in id" do
+        get :show, { :id => image.id }
+
+        image_response = parse_json[:image]
+        expect(image_response[:id]).to eq image.id
+      end
+      specify "with an uploaded filename" do
+        get :show, { :id => image.id }
+        image_response = parse_json[:image]
+        expect(image_response[:uploaded_filename]).not_to be_nil
+      end
+      specify "with an original filename" do
+        get :show, { :id => image.id }
+        image_response = parse_json[:image]
+        expect(image_response[:original_filename]).not_to be_nil
+      end
+    end
+
   end
 end
